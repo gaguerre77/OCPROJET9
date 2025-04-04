09aaff78760f60884020a600eb8e01d777ff2094
Georges <georges.aguerre@wanadoo.fr>
2025-04-04 08:50:16 +0200

    changement F02

--- force-app/main/default/lwc/orders/orders.js
+++ force-app/main/default/lwc/orders/orders.js
@@ -1,15 +1,16 @@
-import { LightningElement, api } from "lwc";
-// TODO - récupérer la méthode apex permettant de faire ce calcul
+import { LightningElement, api, wire } from "lwc";
+import getSumOrdersOfAccount from "@salesforce/apex/OrderController.getSumOrdersOfAccount";
 
 export default class Orders extends LightningElement {
   sumOrdersOfCurrentAccount;
   @api recordId;
 
-  connectedCallback() {
-    this.fetchSumOrders();
-  }
-
-  fetchSumOrders() {
-    // TODO - récupérer le montant total des Orders sur le compte avec la méthode apex
+  @wire(getSumOrdersOfAccount, { accountId: "$recordId" })
+  wiredSumOrders({ error, data }) {
+    if (data) {
+      this.sumOrdersOfCurrentAccount = data;
+    } else if (error) {
+      console.error("Error fetching sum of orders:", error);
+    }
   }
 }
--- force-app/main/default/triggers/CalculMontant.trigger
+++ force-app/main/default/triggers/CalculMontant.trigger
@@ -1,4 +1,4 @@
 trigger CalculMontant on Order(before update) {
-  Order newOrder = Trigger.new[0];
-  newOrder.NetAmount__c = newOrder.TotalAmount - newOrder.ShipmentCost__c;
+  //Order newOrder = Trigger.new[0];
+  //newOrder.NetAmount__c = newOrder.TotalAmount - newOrder.ShipmentCost__c;
 }
